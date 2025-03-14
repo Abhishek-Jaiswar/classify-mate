55290613461d5073a725c04cb1f505a4a7ccb68a
Abhishek Jaiswar <abhishekndxw@gmail.com>
2025-03-14 18:38:22 +0530

    Remove unused files and configurations
    This commit deletes various files including .gitignore, package.json, and multiple component files, as well as configuration files for ESLint, PostCSS, and TypeScript. The removal of these files is part of a cleanup process to streamline the project structure.

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+import { connect } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await connect(
+      process.env.MONGODB_URI || "mongodb://localhost:27017/eduSync"
+    );
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
